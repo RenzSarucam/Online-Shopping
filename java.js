3fe6ecf1ca76b238cd24e1eb943341b93662c655
Kuroko <133122584+RenzSarucam@users.noreply.github.com>
2023-11-13 22:31:24 +0800

    Online-Shopping

--- java.js
+++ java.js
@@ -3,3 +3,11 @@ const header = document.querySelector("header")
 window.addEventListener("scroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY > 0)
 })
+
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector(".nav-menu");
+
+menu.onclick = () => {
+    menu.classList.toggle ("bx-x");
+    navmenu.classList.toggle ("open");
+}
